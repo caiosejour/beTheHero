40ad5e60b83e27a4a938f2f99f79e40d928f1020
Caio Séjour <caiosejour@gmail.com>
2020-03-29 02:00:59 -0300

    Incluindo o diretório build ao reopositório para realzar o deploy.

---
+++ frontend/build/precache-manifest.5dc1a1845af7cf4106a3edf252ca0ee3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf19fee843260db5f641d6f8caea0611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15335d98e1797ca96dd1",
+    "url": "/static/css/main.4e34f41d.chunk.css"
+  },
+  {
+    "revision": "19d7d3b4baacab3db35d",
+    "url": "/static/js/2.2b93c1ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b93c1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15335d98e1797ca96dd1",
+    "url": "/static/js/main.e374834b.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/static/media/logo.7eea718b.svg"
+  }
+]);
